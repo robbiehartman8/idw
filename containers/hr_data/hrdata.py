bcc2b9de233ddc5627d19493a5e45850848d5cec
Robert Hartman <robbie.hartman8@gmail.com>
2022-08-26 00:17:27 -0400

    commit

--- containers/hr_data/hrdata.py
+++ containers/hr_data/hrdata.py
@@ -10,7 +10,7 @@ import traceback
 import os
 
 #create flask app
-torIPAddresses = flask.Flask(__name__)
+hrdata = flask.Flask(__name__)
 
 #database connection details
 sql_server = os.getenv("sql_server")
@@ -19,10 +19,10 @@ sql_password = os.getenv("sql_password")
 sql_database = os.getenv("sql_database")
 
 #define api operations and path
-@torIPAddresses.route('/api/v1/hrdata', methods=["GET", "POST"])
+@hrdata.route('/api/v1/hrdata', methods=["GET", "POST"])
 def processRequest():
     if request.method == "GET":
         return ['hello']
 
 if __name__ == '__main__':
-    torIPAddresses.run(debug=False, host='0.0.0.0')
+    hrdata.run(debug=False, host='0.0.0.0')
