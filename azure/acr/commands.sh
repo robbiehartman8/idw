a03b9acb58e41375aed7b177b9638057e52129d1
Robert <robbie.hartman8@gmail.com>
2022-10-04 13:39:47 -0400

    adf

--- azure/acr/commands.sh
+++ azure/acr/commands.sh
@@ -11,3 +11,19 @@ docker tag worker_api-worker idw234092380480.azurecr.io/apis/worker
 docker push idw234092380480.azurecr.io/apis/example
 docker push idw234092380480.azurecr.io/apis/hrdata
 docker push idw234092380480.azurecr.io/apis/worker
+
+
+#login to acr
+TOKEN=$(az acr login --name usedadvcrdacr05 --expose-token --output tsv --query accessToken)
+docker login usedadvcrdacr05.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password $TOKEN
+
+#tag images
+docker tag example_api-example_api usedadvcrdacr05.azurecr.io/apis/example
+docker tag hr_data_api-hrdata usedadvcrdacr05.azurecr.io/apis/hrdata
+docker tag worker_api-worker usedadvcrdacr05.azurecr.io/apis/worker
+
+#push docker images
+docker push usedadvcrdacr05.azurecr.io/apis/example
+docker push usedadvcrdacr05.azurecr.io/apis/hrdata
+docker push usedadvcrdacr05.azurecr.io/apis/worker
+
