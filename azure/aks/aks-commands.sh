0d970f620a2adae20345047149d2efbf55c904c6
Robert Hartman <robbie.hartman8@gmail.com>
2022-10-04 22:25:59 -0400

    commit updated data

--- azure/aks/aks-commands.sh
+++ azure/aks/aks-commands.sh
@@ -1,10 +1,8 @@
-az account set --subscription 1ae594a5-c5fc-497a-9ff4-2156e90286be
+kubectl apply -f deployment.yaml --namespace ingress-basic
 
-kubectl apply -f deployment.yaml --namespace idw
+kubectl apply -f ingress.yaml --namespace ingress-basic
 
-kubectl apply -f ingress.yaml --namespace idw
-
-kubectl --namespace idw get services
+kubectl --namespace ingress-basic get services
 
 kubectl autoscale deployment {deploymentName} --cpu-percent=30 --min=1 --max=5
 
